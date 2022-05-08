a9abc267aa8b0c543eca36c6fd8a3793f0d01f11
Bui Manh Tien <tienbm@Buis-MacBook-Air.local>
2022-05-08 15:10:03 +0700

    spring boot docker build

---
+++ hello-world-example/src/main/java/com/tienbm/helloworldexample/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.learning.hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.web.bind.annotation.RequestMapping; import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
